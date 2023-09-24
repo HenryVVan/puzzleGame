a3421a7303596758744a92a9c98612936bf3c7c9
henry wan <wanhengiot@163.com>
2023-09-25 01:35:02 +0800

    add image

--- src/com/henry/ui/GameJFrame.java
+++ src/com/henry/ui/GameJFrame.java
@@ -10,13 +10,45 @@ public class GameJFrame extends JFrame {
     public GameJFrame() throws HeadlessException {
         // 设置界面宽、高
 
+        // 初始化界面
         initJFrame();
 
+        // 初始化菜单
         initJMenu();
+
+        // 初始化图片
+        initImage();
+
 
         this.setVisible(true);
 
     }
+
+    private void initImage() {
+        // 初始化图片
+
+/*        // 创建一个图片 ImageIcon 对象
+        ImageIcon imageIcon = new ImageIcon("D:\\Henry_work\\Code\\Java\\out\\puzzleGame\\image\\animal\\animal1\\2.jpg");
+        // 创建一个JLabel对象 (管理容器)
+        JLabel jLabel = new JLabel(imageIcon);
+        // 指定图片位置
+        jLabel.setBounds(0, 0, 105, 105);*/
+
+        for (int i = 0; i < 4; i ++) {
+            for (int j = 0; j < 4; j ++) {
+                if (i == 3 && j == 3) break;
+                // 左上角为原点，其他为坐标
+                JLabel jLabel = new JLabel(new ImageIcon("D:\\Henry_work\\Code\\Java\\out\\puzzleGame\\image\\animal\\animal1\\" + (i * 4 + j + 1) + ".jpg"));
+                jLabel.setBounds(j * 105, i * 105, 105, 105);
+                this.getContentPane().add(jLabel);
+            }
+        }
+
+        // 把管理容器添加到界面中
+        // this.add(jLabel);
+        // this.getContentPane().add(jLabel);
+    }
+
 
 
     private void initJMenu() {
@@ -57,6 +89,9 @@ public class GameJFrame extends JFrame {
         this.setAlwaysOnTop(true);
         // 保证当前窗口居中
         this.setLocationRelativeTo(null);
+
+        // 取消JFrame容器中默认的居中方式，只有取消了设置的其他管理容器才会按X Y放置
+        this.setLayout(null);
 
         // 设置关闭模式 设置为2的话，需要同时在每个JFrame中设置，然后关闭所有的窗口 之后 会退出程序
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
